1467d8575059654d1beab93cea06dd5c4be21b43
paydaypay1 <paydaypay1@gmail.com>
2026-05-23 00:16:42 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 
 import App from './App'
-import GlobalActivityListener from '../config/GlobalEventListener'
-
-GlobalActivityListener()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
